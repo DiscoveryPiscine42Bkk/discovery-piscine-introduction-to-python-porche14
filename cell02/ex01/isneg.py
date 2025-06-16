5ee37c4eabd9d4bba59b6620f87f0bc9e966dc60
porche14 <porcheno639@gmail.com>
2025-06-16 17:14:42 +0000

    add_number

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number: "))
+if number < 0:
+     print("this number is negetive. ")
+elif number > 0:
+     print("this number is positive. ")
+else : 
+     print("this number is both positive and negetive. ")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,5 @@
+password = input("enter your password: ")
+if password == "python is awesome":
+    print("ACCESS GRANTED. ")
+else : 
+    print("ACCESS DENIED. ")
