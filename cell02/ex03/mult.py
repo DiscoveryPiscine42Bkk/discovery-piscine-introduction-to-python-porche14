1834833e0f6ad491771bf6ecf96d35f8076cfff2
porche14 <porcheno639@gmail.com>
2025-06-17 03:24:09 +0000

    mult

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,7 +1,12 @@
-x = float(lnput("enter the frist number: "))
+x = float(input("enter the frist number: "))
 print("x")
-y = float (lnput("enter the second number:"))
-print("y")
+y = float (input("enter the second number:"))
+print(y)
 z = x*y
 print(x,"x",y, "=", z )
-if z 
+if z > 0:
+    print("the result is positive.")
+elif z < 0:
+    print("the result is negetive.")
+else :
+    print("the result is both positive and negetive.")
