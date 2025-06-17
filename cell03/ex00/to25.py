9e3b3a7d3c9889624cf21aae8e9b4cc85e8320df
porche14 <porcheno639@gmail.com>
2025-06-17 03:45:34 +0000

    to25

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+number = float(input("enter a number less than 25: "))
+print(number)
+if number > 25:  
+    print("Error")
+else:
+    while number <= 25:
+        print("inside the loop , my variable is (number)")
