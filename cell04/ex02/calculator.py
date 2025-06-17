e215c70b8f63d8a067946fda6f02c88f644d5468
porche14 <porcheno639@gmail.com>
2025-06-17 07:51:09 +0000

    calculator

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+x = int(input("Give me the first number: "))
+y = int(input("Give me the second number: "))
+print("Thank you!")
+บวก = x+y
+ลบ = x-y
+หาร = x/y
+คุณ = x*y
+print(x,"+",y,"=",บวก)
+print(x,"-",y,"=",ลบ)
+print(x,"/",y,"=",หาร)
+print(x,"*",y,"=",คุณ)
